38bd0bb01736eadc544725e727f83fe73ca35fdd
aathmapriyaa <aathmapriyaa@gmail.com>
2025-08-07 21:02:08 +0530

    project created

---
+++ persons_weightfinderapp.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as pt 
+import sklearn.linear_model as lm
+from sklearn.preprocessing import LabelEncoder
+mydata = pd.read_csv("persons_weight_finder_app.csv")
+le= LabelEncoder()
+mydata["Gender_encoded"] = le.fit_transform(mydata[["Gender"]])
+mydata["BodyType_encoded"] = le.fit_transform(mydata[["BodyType"]])
+x=mydata[["Age","Gender_encoded","BodyType_encoded","Height"]]
+y=mydata["Weight"]
+model=lm.LinearRegression()
+model.fit(x,y)
+print("Coefficients:", model.coef_)
+print("Intercept:", model.intercept_)
+
+print(model.predict([[25,2,3,170]]))  # Example input for prediction
+
